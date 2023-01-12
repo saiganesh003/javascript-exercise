6378d9a5d388f106a25e46bdc46954503611d153
saiganesh003 <105593948+saiganesh003@users.noreply.github.com>
2023-01-12 13:29:02 +0530

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(name,n) {
+  let a='';
+  while(n>0){
+    a+=name;
+    n--;
+  }
+  return a;
+  
 };
 
 // Do not edit below this line
